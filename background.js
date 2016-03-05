ce46a0e6986b2def72e469f6ab7e7e7949fe9f0f
Gordian Zhang <gordianzhang@yahoo.com>
2016-03-05 19:02:33 +0800

    Change from pageAction to browserAction, so that icon is always enabled

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	chrome.pageAction.show(tabId);
 	if (tab.url.indexOf('https://store.playstation.com') === 0) {
 		chrome.storage.local.get(function(cache) {
 			if (!(cache && cache.count)) {
@@ -9,8 +8,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 	}
 });
 
-chrome.pageAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript({file: 'content.js'}, function() {
-		chrome.tabs.create({ url: chrome.extension.getURL('popup.html') });
-	});
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.create({ url: chrome.extension.getURL('popup.html') });
 });
