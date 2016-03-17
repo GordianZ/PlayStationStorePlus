e6f5e289dfb17e67575786382a26e76bbd6e0e20
Gordian Zhang <gordianzhang@yahoo.com>
2016-03-18 00:58:59 +0800

    Fix cache not updating bug

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 	if (tab.url.indexOf('https://store.playstation.com') === 0) {
 		chrome.storage.local.get(function(cache) {
-			if (!(cache && cache.count)) {
+			if (!(cache && cache.count && (Date() - cache.timstamp < 5000))) {
 				chrome.tabs.executeScript({file: 'content.js'});
 			}
 		});
